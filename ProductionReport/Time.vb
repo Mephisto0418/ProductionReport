df74dc6e45425e5e17ca6abd1b9bcff2cfe8d860
Boris_Li <Boris_Li@unimicron.com>
2024-04-02 09:43:11 +0800

    [2.1.24.04.02.1] 菜單欄時間按鈕目前只要按下去就會複製當前時間到剪貼簿

--- ProductionReport/Time.vb
+++ ProductionReport/Time.vb
@@ -2,6 +2,7 @@
 
     Private Sub Time_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TxtTime.Text = Now.ToString("yyyy-MM-dd HH:mm:ss")
+        Clipboard.SetText(TxtTime.Text)
     End Sub
     Private Sub TimerClock_Tick(sender As Object, e As EventArgs) Handles TimerClock.Tick
         TxtTime.Text = Now.ToString("yyyy-MM-dd HH:mm:ss")
